594f0f763518015c1a2242a3481ebbd39d3a5614
Filippe Melo <filippemelor@gmail.com>
2025-01-30 20:28:26 -0300

    feat: adding user entity and repository

---
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserWriteOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MyRecipeBook.Domain.Repositories.User;
+
+public interface IUserWriteOnlyRepository
+{
+    public Task Add(Entities.User user);
+}
